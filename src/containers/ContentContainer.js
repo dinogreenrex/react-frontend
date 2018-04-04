e1bc7cf4f77388b2569bee20e40e42bfe0578783
dinogreenrex <=>
2018-04-04 19:38:23 +0300

    Added modifications for Abstraction

--- src/containers/ContentContainer.js
+++ src/containers/ContentContainer.js
@@ -1,74 +1,81 @@
 import React, {Component} from 'react' ;
-import Person from './Person';
 import Relations from './PersonRelations'
-import PersonAddressList from './PersonAddressList'
 import {connect} from 'react-redux'
 import {Route,withRouter} from 'react-router-dom'
-import AuthRoute from '../components/presentational/AuthRoute'
-import PersonContainer from './PersonContainer'
-import PersonList from './PersonList'
 import {PersonAddressListColumns, PersonListColumns} from '../components/ListColumnDefinition'
 import {Row,Col} from 'antd'
+import {AbstractList} from './AbstractList'
+import {TableToolbar} from '../components/presentational/TableToolbar'
+import {EditRecordContainer} from './EditRecordContainer'
+import {SimpleForm} from '../components/presentational/SimpleForm'
+
 class ContentContainer extends React.Component {
-    constructor(props)
-    {
-        super(props)
+  constructor(props) {
+    super(props)
+  }
 
+  crudSettings() {
+    return {
+      Person: {
+        model: 'Person',
+        url: 'http://localhost/api/koalas',
+        columns: PersonListColumns
+      },
+      PersonAddress: {
+        model: 'PersonAddress',
+        url: 'http://localhost/api/PersonAddress',
+        columns: PersonAddressListColumns
+      }
+    }
+  }
+
+  render(){
+
+    const crudSettings = this.crudSettings()
+    const commonSettings = {
+      dispatch: this.props.dispatch
     }
 
-    render(){
-        const personParams = {
-          url: 'http://localhost/api/koalas',
-          columns: PersonListColumns,
-          abstractResult: this.props.abstractResult ? this.props.abstractResult : null,
-          abstractError: this.props.abstractError ? this.props.abstractError : null,
-          fetchInProgress: this.props.fetchInProgress,
-          currentRecord: this.props.currentRecord ? this.props.currentRecord : null,
-          dispatch: this.props.dispatch,
-        }
-        const personAddressParams = {
-          url: 'http://localhost/api/PersonAddress',
-          columns: PersonAddressListColumns,
-          abstractResult: this.props.abstractResult ? this.props.abstractResult : null,
-          abstractError: this.props.abstractError ? this.props.abstractError : null,
-          fetchInProgress: this.props.fetchInProgress,
-          currentRecord: this.props.currentRecord ? this.props.currentRecord : null,
-          dispatch: this.props.dispatch,
-        }
-       console.log(this.props.match)
-        let isAuth = 1;
-        return (
-            <div className="content" style={{width: '100%'}}>
-                <Route exact
-                   path="/Relations"
-                   component={Relations} />
+//              <SimpleForm selectedRecordId={this.props}/>
+    console.log(this.props.match)
+      let isAuth = 1;
+      return (
+          <div className="content" style={{width: '100%'}}>
+              <Route exact
+                 path="/Relations"
+                 component={Relations} />
 
-                <AuthRoute
-                  exact
-                  path="/Person"
-                  isAuth={isAuth}
-                  component={PersonContainer}
-                />
 
-                <Route exact
-                   path="/Address"
-                   render={() => <PersonAddressList {...personAddressParams} />}
-                />
+              <Route exact
+                 path="/Address"
+                 render={() => <AbstractList
+                   {...crudSettings.PersonAddress}
+                   {...this.props.PersonAddress}
+                   {...commonSettings}
+                   {...this.props.JingaForms}
+                 />}
+              />
 
-                <Route exact
+            <Route exact
                    path="/TabTests"
-                   render={() => <PersonList {...personParams} />}
-                />
-            </div>
-        )
-    }
+                   render={() => <AbstractList
+                     {...crudSettings.Person}
+                     {...this.props.Person}
+                     {...commonSettings}
+                     {...this.props.JingaForms}
+                   />}
+            />
+          </div>
+      )
+  }
 }
 export default withRouter(connect(
   state => {
     return {
-      abstractResult: state.freducer.abstractResult,
-      abstractError: state.freducer.abstractError,
-      fetchInProgress: state.freducer.fetchInProgress,
+      Person: state.personReducer,
+      PersonAddress: state.personAddressReducer,
+      JingaForms: state.jingaForms,
+
     }
   }
 )(ContentContainer))
